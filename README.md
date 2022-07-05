7af729ff6a7c6275eaa6b883f5c4d38c11cfbce5
Jordan Weaver <jweaver@indeed.com>
2022-07-05 12:58:29 -0500

    More fixes, make installable package, add README

---
+++ README.md
@@ -0,0 +1,15 @@
+# mjauto
+
+To install, clone repo and run the following command inside the repo dir:
+
+```
+pip install -e .
+```
+
+Before doing that, you'll likely want to install it into a virtual environment
+versus your main Python distro.
+
+1. `pip install virtualenv`
+2. `virtualenv -p /your/personal/path/to/python3.9 devenv`
+3. `source devenv/bin/activate`
+4. Finally, `pip install -e .`
--- mjauto/cli.py
+++ mjauto/cli.py
@@ -1,31 +1,28 @@
 """
-To use: `python generate_prompts.py -n 5 -c red,purple -s 1,2,3,-1 -ro 1 -sd 5339 -ss 1`
+Example usage: 
+    mjauto -n 5 -p 1,2,3,-1 -d 1 -s 1 -sd 5339 -ss 1 -e " --sameseed"
+
+See `mjauto --help` for more info.
 """
+import glob
 import random
 import time
 
-# FIXME: replace with CLI that looks in directory for .txt files
-CATEGORIES = (
-    'artists',
-    'styles',
-    'themes',
-    'qualifiers',
-)
 
-
-def _read_wordbanks(*categories):
+def _read_wordbanks():
+    """Assumes CLI is run from the word banks directory. """
+    paths = sorted(glob.glob("[0-9]*.txt"))
     banks = []
-    for cat in categories:
-        with open(f'{cat}.txt') as f:
-            lines = f.readlines()
+    for path in paths:
+        with open(path) as bankfile:
+            lines = bankfile.readlines()
         banks.append([item.strip() for item in lines])
 
     return banks
 
 
-def _build_prompt(partials, seed, delineation, shuffle, extras):
+def _build_prompt(partials, seed, delineation, shuffle, extras, elements):
     prompt = []
-    elements = _read_wordbanks(*CATEGORIES)
     for el, partial in zip(elements, partials):
         if partial == -1:
             partial = random.randint(1, max(1, min(-(partial), len(el))))
@@ -57,7 +54,9 @@ def _build_prompt(partials, seed, delineation, shuffle, extras):
 def _parse_args():
     import argparse
 
-    parser = argparse.ArgumentParser(description="Midjourney Prompt Generation CLI")
+    parser = argparse.ArgumentParser(
+        description="Midjourney Prompt Generation CLI"
+    )
     parser.add_argument(
         "-n", "--number", 
         default=10, 
@@ -80,20 +79,37 @@ def _parse_args():
               "be no separation at all. If set to 1, every partial will be "
               "hard separated (i.e. no separation or hard separated with ::)")
     )
-
-    # By default (None), all partials from categories will come in the order they appear in the Categories List
-    # If set to 1, all of the partials are randomized before adding extras
-    parser.add_argument("-s", "--shuffle", default=None)
-    # If provided, sets the seed for all of the prompts
-    parser.add_argument("-sd", "--seed", default=None)
-    # By default, sets the same random seed for all prompts if a --seed isn't provided
-    # If set to 0 and a --seed isn't provided, randomizes the seed for every prompt
-    parser.add_argument("-ss", "--samesies", default=1, type=int)
-    # Used to optionally add anything you want included with every prompt
-    # Note: The argument should be encapsulated by double quotes and should almost definitely have a space
-        # at the beginning so that the parser doesn't strip out everything after the double --
-        # Ex: `-e " --sameseed"`
-    parser.add_argument("-e", "--extras", default=None)
+    parser.add_argument(
+        "-s", "--shuffle", 
+        default=None, 
+        help=("By default (None), all partials from categories will come in " 
+              "the order they appear in the word banks directory. If set to 1, "
+              "all of the partials are randomized before adding extras")
+    )
+    parser.add_argument(
+        "-sd", "--seed", 
+        default=None, 
+        help="If provided, sets the seed for all of the prompts"
+    )
+    parser.add_argument(
+        "-ss", "--samesies", 
+        default=1, 
+        type=int,
+        help=("By default, sets the same random seed for all prompts if a " 
+              "--seed isn't provided. If set to 0 and a --seed isn't "
+              "provided, randomizes the seed for every prompt")
+    )
+    parser.add_argument(
+        "-e", "--extras", 
+        default=None,
+        help="""
+        Used to optionally add anything you want included with every
+        prompt. Note: The argument should be encapsulated by double quotes
+        and should almost definitely have a space at the beginning so that
+        the parser doesn't strip out everything after the double --: 
+        `-e " --sameseed"`
+        """
+    )
 
     return parser.parse_args()
 
@@ -111,14 +127,19 @@ def _generate(args):
     extras = args.extras.split(',') if args.extras is not None else None
     delineation = args.delineation
 
+    # Read in word bank elements
+    elements = _read_wordbanks()
+
     # FIXME: change the filenames to include passed in CLI params
     with open(f'prompts_out-{int(time.time())}.txt', 'w') as file:
         for _ in range(args.number):
             if not args.samesies:
                 seed = random.randint(0, 2^32)
-            file.write(
-                _build_prompt(partials, seed, delineation, shuffle, extras) + "\n"
+
+            prompt = _build_prompt(
+                partials, seed, delineation, shuffle, extras, elements
             )
+            file.write(f"{prompt}\n")
 
 
 def main():
